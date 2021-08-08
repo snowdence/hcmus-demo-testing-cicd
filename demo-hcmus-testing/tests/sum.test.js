65741b3ef2226587791cceae42c2e34d40f4a2a8
snowdence <snowdence2911@gmail.com>
2021-08-08 23:28:36 +0700

    Add demo

---
+++ demo-hcmus-testing/tests/sum.test.js
@@ -0,0 +1,33 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function sum_version_2(a, b) {
+  let _a = typeof a === "string" ? +a : a;
+  let _b = typeof b === "string" ? +b : b;
+  return _a + _b;
+}
+
+describe("sum(int, int)", () => {
+  it("should return 5 if given 2 and 3 ", () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+});
+
+describe("sum(int, int)", () => {
+  it("should return 5 if given 2 and 3 ", () => {
+    expect(sum(2, 1)).toBe(3);
+  });
+});
+
+describe("sum(int, string)", () => {
+  it("string should cast to int ", () => {
+    expect(sum(2, "3")).toBe(5);
+  });
+});
+
+describe("sum(int, string)", () => {
+  it("string should cast to int ", () => {
+    expect(sum_version_2(2, "3")).toBe(5);
+  });
+});
